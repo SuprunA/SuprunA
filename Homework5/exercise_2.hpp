dc1f4c34c6f15d995b9cc8be12329f48ac12e6bc
SuprunA <84080444+SuprunA@users.noreply.github.com>
2022-04-25 19:24:16 +0300

    Create exercise_2.hpp

---
+++ Homework5/exercise_2.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <unordered_set>
+#include <queue>
+
+std::string getSentence(std::string &s)
+{
+    std::string punctuationMarks { ".?!" };                              
+    std::string temp; 
+ 
+    for (const char &c : s)                                               
+    {
+        temp.push_back(c);  
+      
+        if (punctuationMarks.find(c) != std::string::npos)                
+        {
+            s.erase(0, temp.size());                                      
+            if (temp[0] == ' ') { temp.erase(0, 1); }                     
+            return temp;                                                 
+        }
+    }
+
+    return {};                                                           
+}
+
+void exercise_2()
+{
+    std::unordered_set<std::string, std::hash<std::string>> sentences;
+    std::string currentString, tempString;    
+  
+    while (std::getline(std::cin, currentString))
+    {
+        if (!currentString.empty())                                       
+        {
+            tempString += currentString + ' ';                           
+                                                                         
+            while (true)
+            {
+                std::string sentence(std::move(getSentence(tempString))); 
+                if (sentence.empty())
+                {
+                    break;                                                
+                }
+                sentences.insert(std::move(sentence));                    
+            }
+        }
+    }
+
+    std::priority_queue<std::pair<size_t, std::string>> q;
+    for (const auto &sentence : sentences)
+    {
+        q.push({ sentence.size(), sentence });
+    }
+    while (!q.empty())
+    {
+        std::cout << q.top().first << ": " << q.top().second << '\n';
+        q.pop();
+    }
+}
