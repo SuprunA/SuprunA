fb55d655df76b4f57f53a7628914c01baf4ab774
SuprunA <84080444+SuprunA@users.noreply.github.com>
2022-05-03 14:42:48 +0300

    Create lesson_1.hpp

---
+++ Homework8/lesson_1.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <optional>
+#include <vector>
+#include <fstream>
+
+struct Person
+{
+    std::string firstname;
+    std::string lastname;
+    std::optional<std::string> patronymic;
+};
+
+struct PhoneNumber
+{
+    int country_code;
+    int city_code;
+    std::string number;
+    std::optional<int> additional_number;
+};
+
+class PhoneBook
+{
+private:
+    std::vector<std::pair<Person, PhoneNumber>> m_data;
+public:
+    PhoneBook(std::istream &file);
+    friend std::ostream& operator<<(std::ostream &out, const PhoneBook &pb);
+    void SortByName();
+    void SortByPhone();
+    std::pair<std::string, std::vector<PhoneNumber>> GetPhoneNumber(const std::string &firstname);
+    void ChangePhoneNumber(const Person &p, const PhoneNumber &pn);
+};
+
+std::optional<std::string> getOptStr(std::string &s);
+std::ostream& operator<<(std::ostream &out, const Person &p);
+bool operator<(const Person &p1, const Person &p2);
+bool operator==(const Person &p1, const Person &p2);
+
+bool operator<(const PhoneNumber &p1, const PhoneNumber &p2);
+std::optional<int> getOptInt(std::string &s);
+std::ostream& operator<<(std::ostream &out, const PhoneNumber &p);
