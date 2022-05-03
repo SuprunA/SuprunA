07bc2f7581faf9b2bf8b367481875c4ddb28b08d
SuprunA <84080444+SuprunA@users.noreply.github.com>
2022-05-03 13:39:17 +0300

    Create main.cpp

---
+++ Homework6/main.cpp
@@ -0,0 +1,13 @@
+#include "exercise_1.hpp"
+#include "exercise_2.hpp"
+#include "exercise_3.hpp"
+
+int main()
+  
+{
+//    exercise_1();
+//    exercise_2();
+    exercise_3();
+
+    return 0;
+}
